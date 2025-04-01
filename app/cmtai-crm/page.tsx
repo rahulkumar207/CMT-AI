dd17e95663ba96ee263fbc5db5fd53c2175b02d2
Rahul Kumar <rk93272722@gmail.com>
2025-04-01 13:22:44 +0530

    crm-features update

--- app/cmtai-crm/page.tsx
+++ app/cmtai-crm/page.tsx
@@ -1,10 +1,8 @@
 
-import Faqs1 from "@/components/sections/Faqs1"
-import Features4 from "@/components/sections/Features4"
+import Features from "@/components/sections/crm-features"
 import CRMHero from "@/components/sections/crm-hero"
 import HowItWork1 from "@/components/sections/HowItWork1"
 import LogoCloud2 from "@/components/sections/LogoCloud2"
-import Pricing2 from "@/components/sections/Pricing2"
 import Testimonial2 from "@/components/sections/Testimonial2"
 
 import Layout from "@/components/layout/Layout"
@@ -16,12 +14,11 @@ export default function Home2() {
             <Layout headerStyle={2} footerStyle={2}>
                 <CRMHero />
                 <LogoCloud2 />
-                <Features4 />
+                <Features />
                 <Testimonial2 />
                
                 <HowItWork1 />
-                <Pricing2 />
-                <Faqs1 />
+              
 
             </Layout>
         </>
