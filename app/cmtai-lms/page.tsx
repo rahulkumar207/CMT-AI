d503054c15fd286bb598086aa0ae60ff365fded2
Rahul Kumar <rk93272722@gmail.com>
2025-04-01 12:52:58 +0530

    cem-hero changes

--- app/cmtai-lms/page.tsx
+++ app/cmtai-lms/page.tsx
@@ -1,14 +1,11 @@
-import Blog2 from "@/components/sections/Blog2"
-import Cta1 from "@/components/sections/Cta1"
+
 import Faqs1 from "@/components/sections/Faqs1"
 import Features4 from "@/components/sections/Features4"
 import Features5 from "@/components/sections/Features5"
-import Hero2 from "@/components/sections/Hero2"
+import CRMHero from "@/components/sections/crm-hero"
 import HowItWork1 from "@/components/sections/HowItWork1"
 import LogoCloud2 from "@/components/sections/LogoCloud2"
 import Pricing2 from "@/components/sections/Pricing2"
-import Static1 from "@/components/sections/Static1"
-import Team2 from "@/components/sections/Team2"
 import Testimonial2 from "@/components/sections/Testimonial2"
 
 import Layout from "@/components/layout/Layout"
@@ -18,18 +15,15 @@ export default function Home2() {
         <>
 
             <Layout headerStyle={2} footerStyle={2}>
-                <Hero2 />
+                <CRMHero />
                 <LogoCloud2 />
                 <Features4 />
                 <Features5 />
                 <Testimonial2 />
-                <Static1 />
-                <Cta1 />
-                <Team2 />
+               
                 <HowItWork1 />
                 <Pricing2 />
                 <Faqs1 />
-                <Blog2 />
 
             </Layout>
         </>
--- components/layout/Menu.tsx
+++ components/layout/Menu.tsx
@@ -23,7 +23,7 @@ export default function Menu() {
 						<div className="dropdown-menu fix">
 							<ul className="list-unstyled">
 								<li className="position-relative z-1 border-bottom">
-									<Link className="dropdown-item position-relative z-1 d-flex align-items-start" href="cmtai-crm">
+									<Link className="dropdown-item position-relative z-1 d-flex align-items-start" href="/cmtai-crm">
 										<img src="/assets/imgs/service-3/Layer 2.svg" alt="CMT AI CRM" width={18} height={18} className="mt-1" />
 
 
@@ -33,7 +33,7 @@ export default function Menu() {
 									</Link>
 								</li>
 								<li className="position-relative z-1 border-bottom">
-									<Link className="dropdown-item position-relative z-1 d-flex align-items-start" href="cmtai-erp">
+									<Link className="dropdown-item position-relative z-1 d-flex align-items-start" href="/cmtai-erp">
 										<img src="/assets/imgs/service-3/Layer 2.svg" alt="CMT AI ERP" width={18} height={18} className="mt-1" />
 
 										<span className="ms-2">
@@ -42,7 +42,7 @@ export default function Menu() {
 									</Link>
 								</li>
 								<li className="position-relative z-1 border-bottom">
-									<Link className="dropdown-item position-relative z-1 d-flex align-items-start" href="cmtai-lms">
+									<Link className="dropdown-item position-relative z-1 d-flex align-items-start" href="/cmtai-lms">
 										<img src="/assets/imgs/service-3/Layer 2.svg" alt="CMT AI LMS" width={18} height={18} className="mt-1" />
 
 										<span className="ms-2">
@@ -131,9 +131,114 @@ export default function Menu() {
 						</div>
 					</li>
 
-					{/* 				
-					<li className="nav-item">
-						<Link className="nav-link fw-bold d-flex align-items-center" href="/page-contact-1">Contact</Link>
+					{/* <li className="nav-item dropdown has-mega-menu">
+						<Link className="nav-link fw-bold d-flex align-items-center" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Pages
+						</Link>
+						<div className="mega-menu fix start-50 translate-middle-x">
+							<div className="container">
+								<div className="row">
+									<div className="col-12">
+										<div className="mega-menu-content">
+											<div className="row">
+												
+												
+												<div className="col-2">
+													<div className="mega-menu-list">
+														<p className="text-primary fw-bold mb-1 fs-7">STYLE GUIDE</p>
+														<ul className="list-unstyled">
+															<li><Link className="dropdown-item position-relative z-1" href="/page-style-guide">Color  Typography</Link></li>
+															<li><Link className="dropdown-item position-relative z-1" href="/page-elements">Basic elements</Link></li>
+														</ul>
+													</div>
+													<div className="position-relative bg-4 rounded-3 mt-5">
+														<img className="rounded-3" src="/assets/imgs/template/bg-card.png" alt="" />
+														<div className="position-absolute top-50 start-50 translate-middle z-0">
+															<img className="ms-3" src="/assets/imgs/logo/logo-dark.svg" alt="infinia" />
+															<p className="fs-7 fw-bold text-900 text-center my-3 text-nowrap">Multi-layout SaaS  <br /> Startup Website <br /> Template</p>
+															<div className="d-flex mt-3 ms-3">
+																<img src="/assets/imgs/other/avatar-1.png" alt="infinia" />
+																<img className="avt-hero" src="/assets/imgs/other/avatar-2.png" alt="infinia" />
+																<img className="avt-hero " src=" assets/imgs/other/avatar-3.png" alt="infinia" />
+															</div>
+															<Link href="#" className="btn btn-md btn-gradient fs-8 mt-3 text-nowrap">
+																Get this now
+																<svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} viewBox="0 0 18 18" fill="none">
+																	<path d="M12.5477 11.3147V5.30603H6.53906" stroke="white" strokeWidth="1.06034" strokeLinecap="round" strokeLinejoin="round" />
+																	<path d="M12.3707 5.48276L5.125 12.7284" stroke="white" strokeWidth="1.06034" strokeLinecap="round" strokeLinejoin="round" />
+																</svg>
+															</Link>
+														</div>
+													</div>
+												</div>
+												
+												<div className="col-2">
+													<div className="mega-menu-list">
+														<p className="text-primary fw-bold mb-1 fs-7">STYLE GUIDE</p>
+														<ul className="list-unstyled">
+															<li><Link className="dropdown-item position-relative z-1" href="/page-style-guide">Color  Typography</Link></li>
+															<li><Link className="dropdown-item position-relative z-1" href="/page-elements">Basic elements</Link></li>
+														</ul>
+													</div>
+													<div className="position-relative bg-4 rounded-3 mt-5">
+														<img className="rounded-3" src="/assets/imgs/template/bg-card.png" alt="" />
+														<div className="position-absolute top-50 start-50 translate-middle z-0">
+															<img className="ms-3" src="/assets/imgs/logo/logo-dark.svg" alt="infinia" />
+															<p className="fs-7 fw-bold text-900 text-center my-3 text-nowrap">Multi-layout SaaS  <br /> Startup Website <br /> Template</p>
+															<div className="d-flex mt-3 ms-3">
+																<img src="/assets/imgs/other/avatar-1.png" alt="infinia" />
+																<img className="avt-hero" src="/assets/imgs/other/avatar-2.png" alt="infinia" />
+																<img className="avt-hero " src=" assets/imgs/other/avatar-3.png" alt="infinia" />
+															</div>
+															<Link href="#" className="btn btn-md btn-gradient fs-8 mt-3 text-nowrap">
+																Get this now
+																<svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} viewBox="0 0 18 18" fill="none">
+																	<path d="M12.5477 11.3147V5.30603H6.53906" stroke="white" strokeWidth="1.06034" strokeLinecap="round" strokeLinejoin="round" />
+																	<path d="M12.3707 5.48276L5.125 12.7284" stroke="white" strokeWidth="1.06034" strokeLinecap="round" strokeLinejoin="round" />
+																</svg>
+															</Link>
+														</div>
+													</div>
+												</div>
+												<div className="col-2">
+													<div className="mega-menu-list">
+														<p className="text-primary fw-bold mb-1 fs-7">STYLE GUIDE</p>
+														<ul className="list-unstyled">
+															<li><Link className="dropdown-item position-relative z-1" href="/page-style-guide">Color  Typography</Link></li>
+															<li><Link className="dropdown-item position-relative z-1" href="/page-elements">Basic elements</Link></li>
+														</ul>
+													</div>
+													<div className="position-relative bg-4 rounded-3 mt-5">
+														<img className="rounded-3" src="/assets/imgs/template/bg-card.png" alt="" />
+														<div className="position-absolute top-50 start-50 translate-middle z-0">
+															<img className="ms-3" src="/assets/imgs/logo/logo-dark.svg" alt="infinia" />
+															<p className="fs-7 fw-bold text-900 text-center my-3 text-nowrap">Multi-layout SaaS  <br /> Startup Website <br /> Template</p>
+															<div className="d-flex mt-3 ms-3">
+																<img src="/assets/imgs/other/avatar-1.png" alt="infinia" />
+																<img className="avt-hero" src="/assets/imgs/other/avatar-2.png" alt="infinia" />
+																<img className="avt-hero " src=" assets/imgs/other/avatar-3.png" alt="infinia" />
+															</div>
+															<Link href="#" className="btn btn-md btn-gradient fs-8 mt-3 text-nowrap">
+																Get this now
+																<svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} viewBox="0 0 18 18" fill="none">
+																	<path d="M12.5477 11.3147V5.30603H6.53906" stroke="white" strokeWidth="1.06034" strokeLinecap="round" strokeLinejoin="round" />
+																	<path d="M12.3707 5.48276L5.125 12.7284" stroke="white" strokeWidth="1.06034" strokeLinecap="round" strokeLinejoin="round" />
+																</svg>
+															</Link>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="position-absolute top-0 start-50 translate-middle-x z-0">
+								<img src="/assets/imgs/team-1/bg-line.png" alt="infinia" />
+							</div>
+							<div className="ellipse-rotate-success-sm position-absolute top-0 start-0 translate-middle z-0" />
+							<div className="ellipse-rotate-primary-sm position-absolute top-0 end-0 translate-middle-y z-0" />
+						</div>
 					</li> */}
 				</ul>
 			</div>
