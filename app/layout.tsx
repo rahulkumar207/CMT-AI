730e1ff9b1d13e819b6c6db23bc56bae6e3c990b
Rahul Kumar <rk93272722@gmail.com>
2025-04-05 15:52:10 +0530

    env files sequre

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import type React from "react"
-import "/public/assets/css/vendors/bootstrap.min.css"
-import "/public/assets/css/vendors/swiper-bundle.min.css"
-import "/public/assets/css/vendors/aos.css"
-import "/public/assets/css/vendors/odometer.css"
-import "/public/assets/css/vendors/carouselTicker.css"
-import "/public/assets/css/vendors/magnific-popup.css"
-import "/public/assets/fonts/bootstrap-icons/bootstrap-icons.min.css"
-import "/public/assets/fonts/boxicons/boxicons.min.css"
-import "/public/assets/fonts/satoshi/satoshi.css"
-import "/public/assets/css/main.css"
+import type React from "react";
+import "/public/assets/css/vendors/bootstrap.min.css";
+import "/public/assets/css/vendors/swiper-bundle.min.css";
+import "/public/assets/css/vendors/aos.css";
+import "/public/assets/css/vendors/odometer.css";
+import "/public/assets/css/vendors/carouselTicker.css";
+import "/public/assets/css/vendors/magnific-popup.css";
+import "/public/assets/fonts/bootstrap-icons/bootstrap-icons.min.css";
+import "/public/assets/fonts/boxicons/boxicons.min.css";
+import "/public/assets/fonts/satoshi/satoshi.css";
+import "/public/assets/css/main.css";
 
-import "@/node_modules/react-modal-video/css/modal-video.css"
-import { Inter } from "next/font/google"
-import Script from "next/script"
-import type { Metadata } from "next"
+import "@/node_modules/react-modal-video/css/modal-video.css";
+import { Inter } from "next/font/google";
+import Script from "next/script";
+import type { Metadata } from "next";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
@@ -40,56 +40,67 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://www.cmtai.in/",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
+  const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+  const facebookPixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
+
   return (
     <html lang="en" data-bs-theme="dark">
       <body className={inter.className}>
         {children}
 
         {/* Google Analytics */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-GDCMNQC07W"
-        />
-        <Script id="google-analytics">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-GDCMNQC07W');
-          `}
-        </Script>
-{/* Meta Pixel (Facebook Pixel) */}
-<Script id="facebook-pixel" strategy="afterInteractive">
-  {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '556593014722099');
-    fbq('track', 'PageView');
-  `}
-</Script>
-<noscript>
-  <img
-    height="1"
-    width="1"
-    style={{ display: "none" }}
-    src="https://www.facebook.com/tr?id=556593014722099&ev=PageView&noscript=1"
-  />
-</noscript>
+        {googleAnalyticsId && (
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+            />
+            <Script id="google-analytics">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${googleAnalyticsId}');
+              `}
+            </Script>
+          </>
+        )}
 
+        {/* Meta Pixel (Facebook Pixel) */}
+        {facebookPixelId && (
+          <>
+            <Script id="facebook-pixel" strategy="afterInteractive">
+              {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${facebookPixelId}');
+                fbq('track', 'PageView');
+              `}
+            </Script>
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src={`https://www.facebook.com/tr?id=${facebookPixelId}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          </>
+        )}
       </body>
     </html>
-  )
+  );
 }
