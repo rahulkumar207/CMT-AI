f5f99e290d2fbab5902436c2e3622c900b4cb08e
Rahul Kumar <rk93272722@gmail.com>
2025-04-05 11:36:21 +0530

    g

--- app/web-development/page.tsx
+++ app/web-development/page.tsx
@@ -4,9 +4,9 @@ import Layout from "@/components/layout/Layout"
 import Link from "next/link"
 import { useState } from "react"
 import { Metadata } from "next"
-export const metadata: Metadata = {
-title: "web development"
-}
+// export const metadata: Metadata = {
+// title: "web development"
+// }
 export default function PageServicesDetails2() {
 
 	// Accordion
